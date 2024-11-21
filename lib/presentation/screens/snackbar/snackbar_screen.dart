a0e92ff69bea783d777fb23af8acf7eacd8aa3fc
Yilver Diaz <54851994+PaulMec@users.noreply.github.com>
2024-11-20 23:30:54 -0500

    snackbars dialogs finish

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -15,11 +16,47 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Estas seguro?'),
+        content: const Text(
+            'Occaecat adipisicing sint amet nisi voluptate. Lorem in excepteur quis duis incididunt quis do sint commodo deserunt cillum enim. Mollit est sint dolor elit laboris. Quis aliquip velit commodo sit eiusmod ipsum cillum officia velit in laborum nulla. Consequat pariatur cillum occaecat sunt nostrud.'),
+        actions: [
+          TextButton(
+              onPressed: () => context.pop(), child: const Text('Cancelar')),
+          FilledButton(
+              onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Dialogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text(
+                        'Occaecat sit do eu occaecat irure et adipisicing duis commodo.')
+                  ]);
+                },
+                child: const Text('Licencias usadas')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Mostrar dialogo'))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           label: const Text('Mostrar Snackbar'),
